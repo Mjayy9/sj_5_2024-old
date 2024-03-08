2c15a7653ac4f6d0744821af9d6cc7afed7fdd69
Majkyy <marian.sebora9@gmail.com>
2024-03-08 14:00:30 +0100

    Zmena portfólia

--- portfolio.php
+++ portfolio.php
@@ -9,23 +9,24 @@
                 </div>
             </section>
               <section class="container">
-                <?php
+              <?php
                     $n_rows = 2;
                     $n_cols = 4;
                     $n_portfolio = 1;
-                    for($i = 0; $i<$n_rows; $i++) {
-                      echo('<div class = "row"');
-                        for($j = 0; $j<$n_cols; $j++) {
-                          echo('<div class = "col-25 portfolio text-white text-center" id="portfolio-1'.$n_portfolio.'">');
-                          echo('Web stránka '.$n_portfolio);
-                          $n_portfolio++;
-                          echo('</div>');
+                    for($i = 0; $i<$n_rows;$i++)
+                    {
+                        echo('<div class="row">');
+                        for($j = 0; $j<$n_cols;$j++)
+                        {
+                            echo('<div class="col-25 portfolio text-white text-center" id="portfolio-'.$n_portfolio.'">');
+                            echo('Web stránka '.$n_portfolio);
+                            $n_portfolio++;
+                            echo('</div>');
                         }
                         echo('</div>');
                     }
-                ?>
+              ?>
               </section>   
-
         </main>
         <?php
           include_once('partials/footer.php')
